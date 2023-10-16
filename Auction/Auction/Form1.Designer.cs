7d5eae4e047fd4d1d7ec053e2b447a468e42920c
RLD666 <113790461+RLD666@users.noreply.github.com>
2023-10-16 08:13:13 +0300

    Версия 1 (8:12)
    Создание формы авторизации

--- Auction/Auction/Form1.Designer.cs
+++ Auction/Auction/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Auction
 {
-    partial class Form1
+    partial class Avtorization
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -30,12 +30,159 @@ namespace Auction
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Avtorization));
+            this.vhodbtn = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.справкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ShowBtn = new System.Windows.Forms.Button();
+            this.HideBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.avtor_password = new System.Windows.Forms.TextBox();
+            this.avtor_login = new System.Windows.Forms.TextBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip2 = new System.Windows.Forms.ToolTip(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // vhodbtn
+            // 
+            this.vhodbtn.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.vhodbtn.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.vhodbtn.Location = new System.Drawing.Point(223, 287);
+            this.vhodbtn.Name = "vhodbtn";
+            this.vhodbtn.Size = new System.Drawing.Size(123, 40);
+            this.vhodbtn.TabIndex = 65;
+            this.vhodbtn.Text = "Войти";
+            this.vhodbtn.UseVisualStyleBackColor = false;
+            this.vhodbtn.Click += new System.EventHandler(this.vhodbtn_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(142)))), ((int)(((byte)(189)))));
+            this.menuStrip1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("menuStrip1.BackgroundImage")));
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.справкаToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(578, 24);
+            this.menuStrip1.TabIndex = 64;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // справкаToolStripMenuItem
+            // 
+            this.справкаToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.выходToolStripMenuItem});
+            this.справкаToolStripMenuItem.Name = "справкаToolStripMenuItem";
+            this.справкаToolStripMenuItem.Size = new System.Drawing.Size(68, 20);
+            this.справкаToolStripMenuItem.Text = "Помощь";
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            // 
+            // ShowBtn
+            // 
+            this.ShowBtn.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ShowBtn.Image = ((System.Drawing.Image)(resources.GetObject("ShowBtn.Image")));
+            this.ShowBtn.Location = new System.Drawing.Point(385, 222);
+            this.ShowBtn.Name = "ShowBtn";
+            this.ShowBtn.Size = new System.Drawing.Size(44, 39);
+            this.ShowBtn.TabIndex = 62;
+            this.ShowBtn.UseVisualStyleBackColor = false;
+            // 
+            // HideBtn
+            // 
+            this.HideBtn.Image = ((System.Drawing.Image)(resources.GetObject("HideBtn.Image")));
+            this.HideBtn.Location = new System.Drawing.Point(385, 222);
+            this.HideBtn.Name = "HideBtn";
+            this.HideBtn.Size = new System.Drawing.Size(44, 39);
+            this.HideBtn.TabIndex = 63;
+            this.HideBtn.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(185, 188);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(194, 31);
+            this.label2.TabIndex = 61;
+            this.label2.Text = "Введите пароль";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(185, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(181, 31);
+            this.label1.TabIndex = 60;
+            this.label1.Text = "Введите логин";
+            // 
+            // avtor_password
+            // 
+            this.avtor_password.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.avtor_password.Location = new System.Drawing.Point(185, 222);
+            this.avtor_password.Name = "avtor_password";
+            this.avtor_password.Size = new System.Drawing.Size(194, 39);
+            this.avtor_password.TabIndex = 59;
+            this.avtor_password.UseSystemPasswordChar = true;
+            // 
+            // avtor_login
+            // 
+            this.avtor_login.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.avtor_login.Location = new System.Drawing.Point(185, 125);
+            this.avtor_login.Name = "avtor_login";
+            this.avtor_login.Size = new System.Drawing.Size(194, 39);
+            this.avtor_login.TabIndex = 58;
+            // 
+            // Avtorization
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(578, 397);
+            this.ControlBox = false;
+            this.Controls.Add(this.vhodbtn);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.ShowBtn);
+            this.Controls.Add(this.HideBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.avtor_password);
+            this.Controls.Add(this.avtor_login);
+            this.Name = "Avtorization";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Авторизация";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button vhodbtn;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem справкаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.Button ShowBtn;
+        private System.Windows.Forms.Button HideBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox avtor_password;
+        private System.Windows.Forms.TextBox avtor_login;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ToolTip toolTip2;
     }
 }
 
--- Auction/Auction/Form1.cs
+++ Auction/Auction/Form1.cs
@@ -10,11 +10,41 @@ using System.Windows.Forms;
 
 namespace Auction
 {
-    public partial class Form1 : Form
+    public partial class Avtorization : Form
     {
-        public Form1()
+        public Avtorization()
         {
             InitializeComponent();
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ShowBtn_Click(object sender, EventArgs e)
+        {
+            avtor_password.UseSystemPasswordChar = false;
+            ShowBtn.Visible = false;
+            HideBtn.Visible = true;
+        }
+
+        private void HideBtn_Click(object sender, EventArgs e)
+        {
+            avtor_password.UseSystemPasswordChar = true;
+            ShowBtn.Visible = true;
+            HideBtn.Visible = false;
+        }
+
+        private void Avtor_Load(object sender, EventArgs e)
+        {
+            toolTip1.SetToolTip(avtor_login, "Если вы забыли свой логин, то обратитесь к администратору.");
+            toolTip2.SetToolTip(avtor_password, "Если вы забыли свой пароль, то обратитесь к администратору.");
+        }
+
+        private void vhodbtn_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
