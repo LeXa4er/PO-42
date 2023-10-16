1126aa3dc5e00e45724bf35df2fb05c8c3a5de8e
g0do4 <baratov.misha2017@yandex.ru>
2023-10-16 13:56:19 +0300

    Изправление наименований элементов управления

--- Auction/Auction/Form1.cs
+++ Auction/Auction/Form1.cs
@@ -24,22 +24,22 @@ namespace Auction
 
         private void ShowBtn_Click(object sender, EventArgs e)
         {
-            avtor_password.UseSystemPasswordChar = false;
+            TextBoxPass.UseSystemPasswordChar = false;
             ShowBtn.Visible = false;
             HideBtn.Visible = true;
         }
 
         private void HideBtn_Click(object sender, EventArgs e)
         {
-            avtor_password.UseSystemPasswordChar = true;
+            TextBoxPass.UseSystemPasswordChar = true;
             ShowBtn.Visible = true;
             HideBtn.Visible = false;
         }
 
         private void Avtor_Load(object sender, EventArgs e)
         {
-            toolTip1.SetToolTip(avtor_login, "Если вы забыли свой логин, то обратитесь к администратору.");
-            toolTip2.SetToolTip(avtor_password, "Если вы забыли свой пароль, то обратитесь к администратору.");
+            toolTip1.SetToolTip(textBoxLogin, "Если вы забыли свой логин, то обратитесь к администратору.");
+            toolTip2.SetToolTip(TextBoxPass, "Если вы забыли свой пароль, то обратитесь к администратору.");
         }
 
         private void vhodbtn_Click(object sender, EventArgs e)
